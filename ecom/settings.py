99237defb5ab3e50e67818d811e86ca74745a0b6
Krushil Modi <krushimodi363@gmail.com>
2025-01-18 15:05:23 +0530

    DB_PASSWORD updated

--- ecom/settings.py
+++ ecom/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'PASSWORD': os.environ['DB_PASSWORD'],
         'HOST': 'roundhouse.proxy.rlwy.net',
         'PORT': '53152',
     }
