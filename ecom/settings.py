814005db8ee889309e7cb88e1067c764e19d2ea0
Krushil Modi <krushimodi363@gmail.com>
2025-01-18 15:54:06 +0530

    setting.py changes

--- ecom/settings.py
+++ ecom/settings.py
@@ -19,8 +19,8 @@ SECRET_KEY = 'django-insecure-b@@1r%$m8-f5o$7e=k9w_n(9)ou6k74b3wc1clj0y+#!ap$pc^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['complete-ecommerce-django-webapp-production.up.railway.app','https://complete-ecommerce-django-webapp-production.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['https://complete-ecommerce-django-webapp-production.up.railway.app']
+ALLOWED_HOSTS = [ 'localhost', '127.0.0.1', 'complete-ecommerce-django-webapp-production.up.railway.app' ]
+CSRF_TRUSTED_ORIGINS = [ 'https://complete-ecommerce-django-webapp-production.up.railway.app' ]
 
 # Application definition
 
